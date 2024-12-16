400e7ed2c38aa22b312a939db98c658fe07e90f0
huynhtrung1904 <huynhtrung1904@gmail.com>
2024-12-17 02:01:48 +0700

    update - DB

---
+++ database/migrations/2024_12_15_172702_create_prescriptions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prescriptions', function (Blueprint $table) {
+            $table->id('PrescriptionID'); // Primary Key
+            $table->date('PrescriptionDate');
+            $table->foreignId('PatientID')->constrained('patients', 'PatientID')->onDelete('cascade');
+            $table->foreignId('DoctorID')->constrained('doctors', 'DoctorID')->onDelete('cascade');
+            $table->integer('TotalPrice');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prescriptions');
+    }
+};
