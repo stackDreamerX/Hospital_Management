0f11cefd844a0a026c080f635e091db43159c5f7
huynhtrung1904 <huynhtrung1904@gmail.com>
2024-12-31 04:03:41 +0700

    update migration

---
+++ database/migrations/2024_12_31_033841_update_patient_to_user_in_treatments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('treatments', function (Blueprint $table) {
+            // Xóa khóa ngoại và cột cũ
+            $table->dropForeign(['PatientID']);
+            $table->dropColumn('PatientID');
+
+            // Thêm cột mới UserID
+            $table->foreignId('UserID')->after('TreatmentDate')->constrained('users', 'UserID')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('treatments', function (Blueprint $table) {
+            // Xóa khóa ngoại và cột mới
+            $table->dropForeign(['UserID']);
+            $table->dropColumn('UserID');
+
+            // Thêm lại cột PatientID
+            $table->foreignId('PatientID')->constrained('patients', 'PatientID')->onDelete('cascade');
+        });
+    }
+};
